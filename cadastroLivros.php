13346df110b8120b029abdaa0ebddc29e9370ad4
Marcelo Moreira <MarceloLMoreira@users.noreply.github.com>
2020-08-27 17:14:27 -0300

    Arquivos incluídos por upload
    Incluídas a classe Livro e a interface para cadastro de livros

---
+++ cadastroLivros.php
@@ -0,0 +1,36 @@
+<?php
+	require_once('livro.php');
+	
+	
+
+
+
+
+//------------------------------------------------------------------------
+
+
+/*
+<?php
+	//importa outro arquivo php
+	require_once('carro.php');
+	
+	$cor = 'preto ninja';
+	$ano = '2000';
+	$modelo = 'Clio';
+	$marca = 'Renault';
+	
+	$carro = new Carro($cor, $ano, $modelo, $marca);
+	
+	echo 'Cor: ' . $carro->getCor() . PHP_EOL;
+	echo 'Ano: ' . $carro->getAno() . PHP_EOL;
+	echo 'Modelo: ' . $carro->getModelo() . PHP_EOL;
+	echo 'Marca: ' . $carro->getMarca() . PHP_EOL . PHP_EOL;
+
+	$cor = readline("Escolha uma nova cor para o carro: ");	
+	$carro->setCor($cor);
+	
+	echo PHP_EOL . 'Cor: ' . $carro->getCor() . PHP_EOL;
+	echo 'Ano: ' . $carro->getAno() . PHP_EOL;
+	echo 'Modelo: ' . $carro->getModelo() . PHP_EOL;
+	echo 'Marca: ' . $carro->getMarca() . PHP_EOL;
+*/
---
+++ livro.php
@@ -0,0 +1,69 @@
+<?php
+	class Livro{
+		
+		
+	}
+
+
+
+
+
+//-----------------------------------------------------------------------
+
+
+/*
+<?php
+	class Carro{
+		private $cor;
+		private $ano;
+		private $modelo;
+		private $marca;
+		
+		//Método construtor pode ser feito com o nome da própria classe OU com o nome __construct
+		//function __construct($cor, $ano, $modelo, $marca){
+		function Carro($cor, $ano, $modelo, $marca){
+			$this->cor = $cor;
+			$this->ano = $ano;
+			$this->modelo = $modelo;
+			$this->marca = $marca;
+		}
+		//Observar que no $this->atributo coloca-se o $ apenas no início
+		
+		//set
+		public function setCor($cor){
+			$this->cor = $cor;
+		}
+		
+		//get
+		public function getCor(){
+			return $this->cor;
+		}
+		
+		public function setAno($ano){
+			$this->ano = $ano;
+		}
+		
+		public function getAno(){
+			return $this->ano;
+		}
+		
+		public function setModelo($model){
+			$this->modelo = $modelo;
+		}
+		
+		public function getModelo(){
+			return $this->modelo;
+		}
+		
+		public function setMarca($marca){
+			$this->marca = $marca;
+		}
+		
+		public function getMarca(){
+			return $this->marca;
+		}
+		
+		
+	}
+	
+*/
